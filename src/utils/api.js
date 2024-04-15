a56e3ee6156942da95a4cd3276830852d3183411
Huixin Cai <caihuixin2009@hotmail.com>
2024-04-14 22:20:33 -0400

    update api url to backend api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://unit-3-project-api-0a5620414506.herokuapp.com';
-export const API_KEY = '4ec5cacb-66be-4fa4-b97f-00f211dbe2b8';
+export const API_URL = 'http://localhost:8080';
+export const API_KEY = 'secretpassword';
